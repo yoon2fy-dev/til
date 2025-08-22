d2a0165f5897f59445830cd6a3daa68608391b17
JiYoon Yang <yoon2fy@gmail.com>
2025-08-22 19:48:38 +0900

    02: start to Spring-boot

---
+++ src/main/java/ch02/SpringBootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+
+}
