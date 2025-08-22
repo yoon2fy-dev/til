dd21748a0f7350f58f53e768a9144977d9b4322d
JiYoon Yang <yoon2fy@gmail.com>
2025-08-22 20:35:16 +0900

    03: understand about structer

---
+++ src/main/java/TestController.java
@@ -0,0 +1,17 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Member;
+import java.util.List;
+
+@RestController
+public class TestController {
+    @Autowired
+    // TestService testService;
+    @GetMapping("/test")
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
+    }
+}
---
+++ src/main/java/TestService.java
@@ -0,0 +1,2 @@
+public class TestService {
+}
