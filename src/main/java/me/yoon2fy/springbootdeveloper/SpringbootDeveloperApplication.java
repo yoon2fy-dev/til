676846effc97dd5b99ae4e3b77f6f22cd35f22ed
JiYoon Yang <yoon2fy@gmail.com>
2025-08-24 23:00:29 +0900

    6.2: ë¸엔티티 구성하기

---
+++ src/main/java/me/yoon2fy/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.yoon2fy.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDeveloperApplication.class, args);
+    }
+}
