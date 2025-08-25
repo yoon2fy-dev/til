957ccce95282f303dff20efc99a320be0b4d69f3
JiYoon Yang <yoon2fy@gmail.com>
2025-08-25 09:46:31 +0900

    6.5: 글 조회 API 구현

--- src/main/java/me/yoon2fy/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/yoon2fy/springbootdeveloper/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import me.yoon2fy.springbootdeveloper.dto.ArticleResponse;
 import me.yoon2fy.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +34,13 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/article/{id}")
+    public ResponseEntity<ArticleResponse> findArticleById(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
